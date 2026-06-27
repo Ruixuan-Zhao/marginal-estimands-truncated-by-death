b41b85759979cf4cdb12e37f565d448cf34fc2c7
Ruixuan Zhao <149563706+Ruixuan-Zhao@users.noreply.github.com>
2026-06-27 16:22:46 -0400

    Create RealData_1_readSynthetic.R

---
+++ code/Real Data/RealData_1_readSynthetic.R
@@ -0,0 +1,6 @@
+################################################################################
+# Load simulated data for demonstrating the analysis code.
+# These data do not reproduce the real-data results in the manuscript.
+################################################################################
+
+data.RealData <- readRDS("Real Data/example_realdata.rds")
